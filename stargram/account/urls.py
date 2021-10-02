8471563786257690936efa840a5e41c2ee24777b
youngkwon02 <youngkwon02@naver.com>
2021-10-03 00:22:25 +0900

    feat: kakao sign-in user json data get

--- stargram/account/urls.py
+++ stargram/account/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  # path('', views.intro_view, name="intro"),
+  path('login/kakao', views.KakaoSignInView, name="kakao_signin"),
+  path('login/kakao/callback', views.KakaoSignInCallback, name="kakao_signin"),
 ]
--- stargram/account/views.py
+++ stargram/account/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from pathlib import Path
+from django.http.response import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+import json, os, requests
+from django.core.exceptions import ImproperlyConfigured
 
-# Create your views here.
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_FILE = os.path.join(BASE_DIR, 'secrets.json')
+
+
+with open(SECRET_FILE) as f:
+    secrets = json.loads(f.read())
+
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} env variable.".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+REST_API_KEY = get_secret('KAKAO_REST_KEY')
+REDIRECT_URI = "http://localhost:8000/account/login/kakao/callback"
+API_HOST = 'https://kauth.kakao.com/oauth/authorize?client_id='+REST_API_KEY+'&redirect_uri='+REDIRECT_URI+'&response_type=code'
+
+
+def KakaoSignInView(request):
+  return redirect(API_HOST)
+  
+  
+def KakaoSignInCallback(request):
+  CODE = request.GET['code']
+  kakao_token_api = 'https://kauth.kakao.com/oauth/token'
+  data = {
+    'grant_type': 'authorization_code',
+    'client_id': REST_API_KEY,
+    'redirection_uri': 'http://localhost:8000/account/login/kakao/callback',
+    'code': CODE,
+  }
+  
+  token_response = requests.post(kakao_token_api, data=data)
+  access_token = token_response.json().get('access_token')
+  user_info_response = requests.get('https://kapi.kakao.com/v2/user/me', headers={"Authorization": f'Bearer {access_token}'})
+  
+  return JsonResponse({"user_info": user_info_response.json()})
