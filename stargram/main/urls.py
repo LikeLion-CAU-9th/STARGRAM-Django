152eae6f8168bf5c3a06e514061ab71f4346f7c1
MyeongjunKim <ilikerunning@naver.com>
2021-10-03 01:15:25 +0900

    feat: multiple photos upload test

--- stargram/main/urls.py
+++ stargram/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.intro_view, name="intro"),
+  path('upload/', views.upload_view, name="upload"),
 ]
--- stargram/main/views.py
+++ stargram/main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def intro_view(request):
   return render(request, 'intro.html')
+
+def upload_view(request):
+  return render(request, 'upload.html')
