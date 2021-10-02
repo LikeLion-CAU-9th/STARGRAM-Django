3467bafea3f260120041d9a8ed4629ac06f9e2ef
MyeongjunKim <ilikerunning@naver.com>
2021-10-03 01:54:18 +0900

    feat: models

--- stargram/main/models.py
+++ stargram/main/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Content(models.Model):
+    class Meta:
+        verbose_name = 'Content'
+        verbose_name_plural = 'Contents'
+
+    # user = models.ForeignKey(
+    #     User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    date_start = models.DateField()
+    date_end = models.DateField()
+    name = models.CharField(max_length=100, null=False, blank=False)
+    impression = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    class Meta:
+        verbose_name = 'Photo'
+        verbose_name_plural = 'Photos'
+    
+    Content = models.ForeignKey(
+        Content, on_delete=models.SET_NULL, null=True, blank=True)
+    image = models.ImageField(null=False, blank=False)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.description
--- stargram/main/views.py
+++ stargram/main/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Content, Photo
 
 def intro_view(request):
   return render(request, 'intro.html')
 
 def upload_view(request):
   return render(request, 'upload.html')
+
